58f307009cf4a47c552df91941716c131015fb16
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2018-12-14 11:31:27 +0300

    model with conv and lstm

--- utils.py
+++ utils.py
@@ -1,7 +1,27 @@
 import numpy as np
+import tensorflow as tf
 
 
 def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split(' ')
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+def read_glove_6B_vecs(glove_file):
     with open(glove_file, 'r') as f:
         words = set()
         word_to_vec_map = {}
@@ -43,7 +63,7 @@ def pretrained_embedding_layer(word_to_vec_map, word_to_index):
         emb_matrix[index, :] = word_to_vec_map[word]
 
     # Define Keras embedding layer with the correct output/input sizes, make it trainable. Use Embedding(...). Make sure to set trainable=False.
-    embedding_layer = Embedding(vocab_len,emb_dim, trainable=False)
+    embedding_layer = tf.keras.layers.Embedding(vocab_len,emb_dim, trainable=False)
 
     # Build the embedding layer, it is required before setting the weights of the embedding layer. Do not modify the "None".
     embedding_layer.build((None,))
@@ -52,3 +72,43 @@ def pretrained_embedding_layer(word_to_vec_map, word_to_index):
     embedding_layer.set_weights([emb_matrix])
 
     return embedding_layer
+
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()` (described in Figure 4). 
+    
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this. 
+    
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
+    
+    m = X.shape[0]                                   # number of training examples
+    
+    
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape 
+    X_indices = np.zeros(shape=(m,max_len))
+    
+    for i in range(m):                               # loop over training examples
+        
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words =X[i].lower().split()
+        
+        # Initialize j to 0
+        j = 0
+        
+        # Loop over the words of sentence_words
+        for w in sentence_words:
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+                if w in word_to_index:
+                    X_indices[i, j] = word_to_index[w]
+                    # Increment j to j + 1
+                    j = j+1
+            
+    
+    
+    return X_indices
